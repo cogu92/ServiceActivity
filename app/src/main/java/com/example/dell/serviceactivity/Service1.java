95c2eee21af1a8ec42081fd17164ae7de5a106d3
Nicolas <nicolascogu@hotmail.com>
2017-01-31 11:15:49 +0100

    initial comit

--- app/src/main/java/com/example/dell/serviceactivity/Service1.java
+++ app/src/main/java/com/example/dell/serviceactivity/Service1.java
@@ -2,12 +2,42 @@ package com.example.dell.serviceactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class Service1 extends AppCompatActivity {
+public class Service1 extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG ="Service1" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service1);
+
+        Button mbtnStartService = (Button) findViewById(R.id.btn_Start_Service);
+        mbtnStartService.setOnClickListener(this);
+        Button mbtnStartBoundService = (Button) findViewById(R.id.btn_Start_Bound_Service);
+        mbtnStartBoundService.setOnClickListener(this);
+        Button mbtnStartIntentService = (Button) findViewById(R.id.btn_Start_Intent_Service);
+        mbtnStartIntentService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_Start_Service:
+                Log.e(Service1.TAG,"btn_Start_Service");
+                return;
+            case R.id.btn_Start_Bound_Service:
+                Log.v(Service1.TAG,"btn_Start_Bound_Service");
+                return;
+            case R.id.btn_Start_Intent_Service:
+                Log.d(Service1.TAG,"btn_Start_Intent_Service");
+                return;
+        }
+
+
     }
 }
